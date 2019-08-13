09b0d02ade1997ed01b99d0cc9bbfd7963b37870
manny <emmanuel.medina08@gmail.com>
2019-08-13 15:37:39 -0400

    made add,remove,list,read commads

--- app.js
+++ app.js
@@ -1,18 +1,53 @@
 const  getNotes =  require('./notes.js')
+const yargs = require('yargs')
 const chalk = require ('chalk')
 
 
-const command = process.argv[2]
+//customize yargs version
+yargs.version('1.1.0')
 
-if (command === 'add') {
+//create add command 
+yargs.command({
 
-    console.log(process.argv)
-}
-else if ( command === 'remove'){
-   
-    console.log('removing note')
+    command: 'add',
+    describe: 'add  note',
+    handler: function ( ) {
+    console.log('adding a  note')
+    }
+})
+
+//Create Remove Command 
+yargs.command({
+     command: 'remove',
+    describe: 'remove a  note',
+    handler: function ( ) {
+    console.log('removing a note')
+    }
+})
+//create List command
+yargs.command({
+     command: 'List',
+    describe: 'List  a  note',
+    handler: function ( ) {
+    console.log('Listing notes')
+    }
+})
+
+//create read command
+yargs.command({
+     command: 'Read',
+    describe: 'Read  a  note',
+    handler: function ( ) {
+    console.log('Reading notes')
+    }
+})
+
+
+//add, remove, read, list
+console.log(yargs.argv)
+
         
-}
+
 
 
 
