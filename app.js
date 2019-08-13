a04d1729a07e0f8d9e5651c55097e9cbf5a34100
manny <emmanuel.medina08@gmail.com>
2019-08-13 13:53:51 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const  getNotes =  require('./notes.js')
+const chalk = require ('chalk')
+const msg = getNotes()
+
+console.log(msg)
+console.log(chalk.green.bold.underline("Hello Beautiful People"))
+
+
+
+
---
+++ notes.js
@@ -0,0 +1,8 @@
+
+const getNotes =  function () {
+
+   return "My notes...."
+  }
+
+
+module.exports = getNotes
